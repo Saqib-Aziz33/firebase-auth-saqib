afcb72a8d3aba5819939f016ab71f0c2425acd24
Saqib-Aziz33 <saqib.aziz41@gmail.com>
2022-11-09 16:41:42 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// pages
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+// layout
+import Base from "./components/layouts/Base";
+// other
+import "./app.scss";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Base>
+        <Routes>
+          <Route index path="/" element={<Home />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </Base>
+    </BrowserRouter>
+  );
+}
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+// componenets
+import { Box, Button, Container } from "@mui/material";
+
+function Home() {
+  return (
+    <Box>
+      <h1>Home</h1>
+      <Button variant="contained">Welcome to mui</Button>
+    </Box>
+  );
+}
+export default Home;
